06fbd73d51a9889f290e8e946566cf24659f800f
Isaque Nunes <Isaquenun9@gmail.com>
2024-06-23 21:31:43 -0300

    Criando uma função para gerar cada monstro

--- monstros.h
+++ monstros.h
@@ -12,5 +12,21 @@ typedef struct {
 int xp_Monstro(Monstro monstro) {
     return monstro.xp;
 }
-
-Monstro goblin = {"Goblin", }
+// Esta função daqui cria nosso bom e velho goblin
+Monstro criarGoblin()
+{
+    Monstro goblin = {"Goblin",30,10,5,15,20,1};
+    return goblin;
+}
+//Esqueletin gerado
+Monstro criarEsqueleto()
+{
+    Monstro goblin = {"Esqueleto",30,10,5,15,20,1};
+    return goblin;
+}
+// O GIGANTE ESTÁ AQUI GARAIO
+Monstro criarGigante()
+{
+    Monstro goblin = {"Gigante", 300, 50, 35, 25, 200, 10};
+    return goblin;
+}
